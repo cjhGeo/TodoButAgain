7272339c8005e5312720ac8a77dfc27d3ef5e787
cjhGeo <chong.jian.hang.geoffrey@sji.edu.sg>
2022-07-30 09:44:18 +0800

    Initial Commit

---
+++ TodoButAgain/TodoButAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoButAgainApp.swift
+//  TodoButAgain
+//
+//  Created by T Krobot on 30/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoButAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
